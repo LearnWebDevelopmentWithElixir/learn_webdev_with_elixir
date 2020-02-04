a7a7f67169b39c34dea31ab84741a8ed749ec32e
Anil Wadghule <anildigital@gmail.com>
2020-02-04 20:09:07 +0530

    Fixed pool size for Heroku

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :learn_webdev_with_elixir, LearnWebdevWithElixir.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer("10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
